4078433b1a577bdeff22fc3ffd58d92ba2ed847c
Khrutski <khrutski@gmail.com>
2014-10-17 20:10:05 +0300

    Changed User.java

---
+++ app-core/src/test/java/com/epam/brest/courses/domain/UserTest.java
@@ -0,0 +1,32 @@
+package com.epam.brest.courses.domain;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+    User user;
+
+    @Before
+    public void setUp() throws Exception{
+        user = new User();
+    }
+
+    @Test
+    public void testGetUserName() throws Exception {
+
+        user.setUserName("User Name");
+        Assert.assertEquals("User Name", user.getUserName());
+    }
+
+    @Test
+    public void testGetLogin() throws Exception {
+
+        user.setLogin("User login");
+        Assert.assertEquals("User login", user.getLogin());
+    }
+
+}
