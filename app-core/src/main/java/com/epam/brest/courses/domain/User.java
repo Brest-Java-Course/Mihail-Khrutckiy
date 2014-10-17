8a09dd05db02275803f316b1becfbb5b01c3f077
Khrutski <khrutski@gmail.com>
2014-10-17 19:21:22 +0300

    Changed User.java

--- app-core/src/main/java/com/epam/brest/courses/domain/User.java
+++ app-core/src/main/java/com/epam/brest/courses/domain/User.java
@@ -6,13 +6,29 @@ public class User{
 
 	private String login;
 
-	private String userName;
+    private String userName;
 
-	public long getUserId(){
-		return userId;
-	}
+    public long getUserId() {
+        return userId;
+    }
 
-	public void setUserId(long userId){
-		this.userId = userId;
-	}
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
